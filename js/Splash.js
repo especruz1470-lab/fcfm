4ef6262fd64fb443459f07f33039273786c636b4
especruz1470-lab <especruz1470@gmail.com>
2025-11-23 02:30:36 -0600

    Update and rename Splash.js to index.js

--- js/Splash.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  // Espera 7 segundos (duración de tu animación)
-  setTimeout(() => {
-    // Redirige a la página principal
-    window.location.href = "InicioSesion.html";
-  }, 7000); // ajusta según la duración real de tu splash
-
-});
---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  // Espera 7 segundos (duración de tu animación)
+  setTimeout(() => {
+    // Redirige al inicio de sesión
+    window.location.href = "IniciodeSesion.html";
+  }, 7000);
+});
