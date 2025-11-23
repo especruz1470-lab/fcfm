079680a97fafa0d4acd13e3e8b0d8eeaae50c244
especruz1470-lab <especruz1470@gmail.com>
2025-11-23 02:59:04 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 window.addEventListener("load", () => {
-  // Espera 7 segundos (duración de tu animación)
   setTimeout(() => {
-    // Redirige al inicio de sesión
     window.location.href = "IniciodeSesion.html";
   }, 7000);
 });
